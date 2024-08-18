bdd8ea01ad5f9d6fb9da4e0fd0b021b63d6e92e6
Bhankid <alfredfianyo50@gmail.com>
2024-08-18 11:51:10 +0000

    update 1.0.0.2.3

--- src/main/java/com/example/currencyconverter/Main.java
+++ src/main/java/com/example/currencyconverter/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("CurrencyConverter.fxml"));
-        Scene scene = new Scene(root, 600, 500);
+        Scene scene = new Scene(root, 700, 600);
 
         primaryStage.setTitle("Currency Converter");
         primaryStage.setScene(scene);
